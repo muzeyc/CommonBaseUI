c2dbd244c916a4fdb2c63e32baa8a90c9567acd6
zhangmm <zhangmm@dataoneinfo.com>
2018-12-11 17:28:23 +0800

    增加了控件的值变化参数

---
+++ CommonBaseUI/Model/ValueChangedEventArge.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CommonBaseUI.Model
+{
+    public class ValueChangedEventArge : RoutedEventArgs
+    {
+        public ValueChangedEventArge(RoutedEvent routedEvent, object source)
+            : base(routedEvent, source)
+        {
+
+        }
+        /// <summary>
+        /// 变化后的值
+        /// </summary>
+        public object _Value { get; set; }
+        /// <summary>
+        /// 变化前的值
+        /// </summary>
+        public object _ValueBeforeChange { get; set; }
+    }
+}
